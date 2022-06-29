9006d27e7d3f16a52c1948fbae40750dd98785bc
instabidlan <vlad.tsema76@gmail.com>
2022-06-29 04:15:17 +0300

    project_layer fix

--- project_layer.py
+++ project_layer.py
@@ -8,7 +8,7 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(command=['start'])
+@dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     await message.reply("aboba")
 
